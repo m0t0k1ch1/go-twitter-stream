b7274a7aeb977bba6eeb9d38212ef8d970fb3fcf
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-01-24 01:51:42 +0900

    GET or POST

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"bufio"
+	"fmt"
 	"net/http"
 
 	"github.com/mrjones/oauth"
@@ -33,26 +34,36 @@ func NewClient(consumerKey, consumerSecret, accessToken, accessTokenSecret strin
 	}
 }
 
-func (c *Client) NewStream(endpoint string, param map[string]string) (*Stream, error) {
+func (c *Client) NewStream(method, endpoint string, param map[string]string) (*Stream, error) {
 	consumer := oauth.NewConsumer(
 		c.ConsumerKey,
 		c.ConsumerSecret,
 		oauth.ServiceProvider{},
 	)
 
-	res, err := consumer.Post(endpoint, param, c.AccessToken)
+	var resp *http.Response
+	var err error
+
+	switch method {
+	case "GET":
+		resp, err = consumer.Get(endpoint, param, c.AccessToken)
+	case "POST":
+		resp, err = consumer.Post(endpoint, param, c.AccessToken)
+	default:
+		return nil, fmt.Errorf("\"%s\" method is not supported", method)
+	}
 	if err != nil {
 		return nil, err
 	}
 
 	return &Stream{
-		Response: res,
-		Scanner:  bufio.NewScanner(res.Body),
+		Response: resp,
+		Scanner:  bufio.NewScanner(resp.Body),
 	}, nil
 }
 
 func (c *Client) UserStream(param map[string]string) (*Stream, error) {
-	return c.NewStream(UserStreamEndpoint, param)
+	return c.NewStream("GET", UserStreamEndpoint, param)
 }
 
 func (s *Stream) Scan() ([]byte, error) {
